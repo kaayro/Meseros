ea1b68274713fec85618fe74b9e872b4bb0c4a8c
Carlos Celma <solrac@igitsoft.com>
2013-07-29 17:50:10 +0200

    as

--- js/iG-func.dev.js
+++ js/iG-func.dev.js
@@ -3,7 +3,7 @@ $(function(){
     document.addEventListener("deviceready",function(){
         //var listener = self.setInterval(function(){listarMesas()},1500);
         listarCarta();
-        navigator.nnotification.alert('hola',null,'tit','btn');
+        navigator.notification.alert('hola',null,'tit','btn');
         //Ir a pendientes
         $('#btnPendientes').click(function(){
             $('#pedidos').hide();
